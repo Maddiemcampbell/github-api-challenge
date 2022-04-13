25e215e8fa5905ced244f8a07ab11445953d7981
maddiemcampbell <maddiemcampbell@gmail.com>
2022-04-13 07:59:04 -0700

    create component structure

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import Wrapper from "./Wrapper";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Wrapper />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Wrapper.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Wrapper = () => {
+  return (
+    <div>
+      <div className="row center">
+        <input placeholder="Github Key"/>
+        <input placeholder="Repo Name"/>
+        <button>Submit</button>
+      </div>
+    </div>
+  );
+};
+
+export default (Wrapper);
